df1f2c1b2be0a4fe300eabc99dc68d548f4c334c
Kirandwip Paul <kirandwip.info@gmail.com>
2025-03-23 22:22:33 +0530

    completed admin page login and initial dashboard

--- routes.py
+++ routes.py
@@ -8,13 +8,41 @@ from sqlalchemy import func
 from sqlalchemy.exc import SQLAlchemyError
 from flask import send_file
 
+
+
+
+## Decorators
+
+def auth_required(f):
+    @wraps(f)
+    def inner(*args, **kwargs):
+        if 'user_id' not in session:
+            flash('please login to continue')
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return inner
+
+def admin_required(f):
+    @wraps(f)
+    def inner(*args, **kwargs):
+        if 'user_id' not in session:
+            flash('please login to continue')
+            return redirect(url_for('login'))
+        user=User.query.get(session['user_id'])
+        if user.is_admin!=True:
+            flash('You are not authorized to view this page')
+            return redirect(url_for('index'))
+        return f(*args, **kwargs)
+    return inner
+
+## Index page routes 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/login')           
 def login():
-    return render_template('login.html') 
+    return render_template('auth/login.html') 
 
 @app.route('/login', methods=['POST'])
 def login_post():
@@ -23,6 +51,7 @@ def login_post():
     
     if username=='' or password=='':
         flash('Username and password cannot be empty')
+
     user=User.query.filter_by(username=username).first()
     
     if not user:
@@ -37,7 +66,10 @@ def login_post():
     session['user_id']=user.id
     #session['username']=user.username
     flash('Login successful')
-    return redirect(url_for('profile'))
+    if user.is_admin==True:
+        return redirect(url_for('admin_dashboard'))
+    else:
+        return redirect(url_for('profile'))
 
 @app.route('/logout')
 def logout():
@@ -49,7 +81,7 @@ def logout():
 
 @app.route('/register')
 def register():
-    return render_template('register.html')
+    return render_template('auth/register.html')
 
 @app.route('/register', methods=['POST'])
 def register_post():
@@ -70,35 +102,13 @@ def register_post():
     flash('Registration successful. Please Log In')
     return redirect(url_for('login'))
 
-## Decorators
-
-def auth_required(f):
-    @wraps(f)
-    def inner(*args, **kwargs):
-        if 'user_id' not in session:
-            flash('please login to continue')
-            return redirect(url_for('login'))
-        return f(*args, **kwargs)
-    return inner
-
-def admin_required(f):
-    @wraps(f)
-    def inner(*args, **kwargs):
-        if 'user_id' not in session:
-            flash('please login to continue')
-            return redirect(url_for('login'))
-        user=User.query.get(session['user_id'])
-        if user.role!='admin':
-            flash('You are not authorized to view this page')
-            return redirect(url_for('index'))
-        return f(*args, **kwargs)
-    return inner
+## user / student routes 
 
 @app.route('/profile')
 @auth_required 
 def profile():
     user=User.query.get(session['user_id'])
-    return render_template('profile.html',user=user)
+    return render_template('student/profile.html',user=user)
 
 @app.route('/profile', methods=['POST'])
 @auth_required
@@ -134,5 +144,64 @@ def profile_post():
     flash('Profile updated successfully')
     return redirect(url_for('profile'))
 
+@app.route('/scores')
+@auth_required
+def scores():
+    return render_template('scores.html') 
+
+@app.route("/summary")
+@auth_required
+def summary():
+    return render_template('summary.html')
+
+## Admin routes 
+
+@app.route('/admin/dashboard')
+@admin_required
+@auth_required
+def admin_dashboard():
+    return render_template('/admin/admin_dashboard.html')
+
+## profile routes for admin
+'''
+@app.route('/admin/profile')
+@admin_required
+def admin_profile():
+    user=User.query.get(session['user_id'])
+    return render_template('admin/admin_profile.html',user=user)
+
+@app.route('/admin/profile', methods=['POST'])
+@auth_required
+def admin_profile_post():
+    cpassword = request.form.get('cpassword')
+    password = request.form.get('password')
+    
+    if not cpassword or not password :
+        flash('Please fill out all the required fields')
+        return redirect(url_for('admin_profile'))
+    
+    user = User.query.get(session['user_id'])
+    if not check_password_hash(user.passhash, cpassword):
+        flash('Incorrect password')
+        return redirect(url_for('admin_profile'))
+    
+    new_password_hash = generate_password_hash(password)
+    user.passhash = new_password_hash
+
+    db.session.commit()
+    flash('Profile updated successfully')
+    return redirect(url_for('admin_profile'))
+
+'''
+
+@app.route("/admin/summary")
+@admin_required
+def admin_summary():
+    return render_template('admin/admin_summary.html')
+
+@app.route("/admin/manage_quiz")
+@admin_required
+def manage_quiz():
+    return render_template('admin/manage_quiz.html')
 
 
